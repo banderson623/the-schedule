9cda2982426a12b21d0cb9992f4c3b4f57a14ecc
Brian Anderson <banderson623@gmail.com>
2013-08-19 20:53:30 -0500

    trying to get carrier wave configured

--- config/initializers/carrier_wave_config.rb
+++ config/initializers/carrier_wave_config.rb
@@ -1,13 +1,14 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'xxx',                        # required
-    :aws_secret_access_key  => 'yyy',                        # required
-    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-    :host                   => 's3.example.com',             # optional, defaults to nil
-    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+    :aws_access_key_id      => 'AKIAJLAMANY7RLQXLHMQ',       # required
+    :aws_secret_access_key  => '8Wn7Q8PPf4eQURFupecBfZ3daCZurznCBR5Lc2hO',                        # required
+    :region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
+    # :host                   => 's3.example.com',             # optional, defaults to nil
+    :endpoint               => ' the-schedule.s3-website-us-west-2.amazonaws.com' # optional, defaults to nil
   }
-  config.fog_directory  = 'name_of_directory'                     # required
+  # This is teh bucket name
+  config.fog_directory  = 'the-schedule'                          # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
