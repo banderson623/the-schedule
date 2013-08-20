71d646bf9ed2730c4493b4098b7aa2c6147781df
Brian Anderson <banderson623@gmail.com>
2013-08-20 01:01:02 -0500

    files uploading

--- config/initializers/carrier_wave_config.rb
+++ config/initializers/carrier_wave_config.rb
@@ -3,12 +3,16 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => 'AKIAJLAMANY7RLQXLHMQ',       # required
     :aws_secret_access_key  => '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',                        # required
-    :region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
+    :region                 => 'us-west-1',                  # optional, defaults to 'us-east-1'
     # :host                   => 's3.example.com',             # optional, defaults to nil
-    :endpoint               => ' the-schedule.s3-website-us-west-2.amazonaws.com' # optional, defaults to nil
+    # :endpoint               => 'https://s3-us-west-1.amazonaws.com/the-schedule/' 
+                                # optional, defaults to nil
   }
+  
+  # https://s3-us-west-1.amazonaws.com/the-schedule/development/myBackyard-small.jpg
+  
   # This is teh bucket name
   config.fog_directory  = 'the-schedule'                          # required
-  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_public     = true                                    # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
